b781c9b17e124a8ea540354b9b7b7cddbac72ec4
danilmv <43599357+danilmv@users.noreply.github.com>
2021-09-12 14:27:24 +0300

    L6: field

---
+++ app/src/main/java/com/andriod/game15/Game15Adapter.kt
@@ -0,0 +1,43 @@
+package com.andriod.game15
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.andriod.game15.databinding.ItemCellBinding
+
+class Game15Adapter : RecyclerView.Adapter<Game15Adapter.ViewHolder>() {
+    var data = emptyList<Int>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    class ViewHolder(parent: ViewGroup) :
+        RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_cell,
+                parent,
+                false)) {
+        private val binding = ItemCellBinding.bind(itemView)
+
+        fun bind(value: Int) {
+            binding.textView.apply {
+                if (value != 0) {
+                    text = value.toString()
+                    binding.textView.background =
+                        if (value == adapterPosition + 1)
+                            AppCompatResources.getDrawable(context, R.color.green)
+                        else
+                            AppCompatResources.getDrawable(context, R.color.red)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
+
+    override fun getItemCount() = data.size
+}
--- app/src/main/java/com/andriod/game15/MainActivity.kt
+++ app/src/main/java/com/andriod/game15/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.andriod.game15
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.andriod.game15.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(LayoutInflater.from(this)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        prepareField()
+    }
+
+    private fun prepareField() {
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(this@MainActivity, FIELD_SIZE)
+            adapter = Game15Adapter().apply {
+                data = generateData()
+            }
+        }
+    }
+
+    private fun generateData(): List<Int> {
+        val result = mutableListOf<Int>()
+        val map = HashMap<Int, Boolean>()
+        for (i in 0 until FIELD_SIZE * FIELD_SIZE) {
+            do {
+                val value = Random.nextInt(FIELD_SIZE * FIELD_SIZE)
+                if (map[value] == true) continue
+                map[value] = true
+                result.add(value)
+                break
+            } while (true)
+        }
+
+        return result
+    }
+
+    companion object {
+        const val FIELD_SIZE = 4
     }
 }
